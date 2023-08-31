8b0b34a755196a9531bd41d40e57c714ad95bea1
ViniMac3do <142914518+ViniMac3do@users.noreply.github.com>
2023-08-31 20:25:08 -0300

    Add files via upload

---
+++ tpa/WorkSpace-TPA/1BIM/aula10/Exercicio4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+	
+		Scanner in = new Scanner(System.in);
+		int i=2,f=1;
+		
+			while(i<=100) {
+				f=f+i;
+			System.out.println("somatória realizada é " +f);
+			i++;
+		}
+		
+		in.close();
+	}
+
+}
