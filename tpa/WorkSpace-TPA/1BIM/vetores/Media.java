04caa72e90e452eebb85ec1327dba8c0fcc812f4
ViniMac3do <142914518+ViniMac3do@users.noreply.github.com>
2023-10-24 09:50:17 -0300

    Atualizar o Media.java

--- tpa/WorkSpace-TPA/1BIM/vetores/Media.java
+++ tpa/WorkSpace-TPA/1BIM/vetores/Media.java
@@ -6,13 +6,13 @@ public class Media {
 public static void main(String[] args) {
 	Scanner in= new Scanner(System.in);
 	int []a= new int[10];
-	int b =0;
+	double b =0;
 	for (int i = 0; i < a.length; i++) {
-		System.out.println("Digite o valor de numero "+i+"°");
+		System.out.println("Digite o valor de numero "+i+"Â°");
 		a[i]= in.nextInt();
 		b+=a[i];
 	}
-	System.out.println("A media da vetor é de: "+(b/10));
+	System.out.println("A media da vetor Ã© de: "+(b/10));
 	in.close();
 }
 }
